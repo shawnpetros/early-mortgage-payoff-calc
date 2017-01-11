bf00db1e229501333588464804f52f24e583225f
Shawn Petros <arashcuzi@gmail.com>
2017-01-11 11:15:36 -0600

    some more modifications

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { validateFields } from './utils/utils';
+import Snackbar from 'material-ui/Snackbar';
 import Form from './components/form/';
 import logo from './logo.svg';
 import './App.css';
@@ -9,14 +11,48 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 class App extends Component {
+  componentWillMount() {
+    this.state = {
+      salary: '55000',
+      taxRatio: '30',
+      saveRate: '70',
+      homeCost: '150000',
+      yearsBorrowed: '15',
+      mortgageInterest: '2.8',
+      investmentRate: '8'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const key = event.target.id;
+    const val = event.target.value;
+
+    this.setState({
+      [key]: val
+    });
+
+  }
+
   render() {
+    const showValidate = !validateFields(this.state);
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <Form />
+        <p className='App-intro'>
+          Fill in the following
+        </p>
+        <Form state={ this.state } handleChange={this.handleChange} />
+        <Snackbar
+          open={showValidate}
+          message='Please verify your entries, a valid calculation cannot be computed.'
+          autoHideDuration={10000}
+        />
       </div>
     );
   }
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import './form.css';
 
-export default () => (
-  <form>
-    <TextField hintText='Base Salary' floatingLabelText='Base Salary' />
-    <TextField hintText='Taxes and Deductions Ratio' floatingLabelText='Taxes and Deductions Ratio' />
-    <TextField hintText='Savings Rate' floatingLabelText='Savings Rate' />
-    <TextField hintText='Home Cost' floatingLabelText='Home Cost' />
-    <TextField hintText='Years in Mortgage' floatingLabelText='Years in Mortgage' />
-    <TextField hintText='Interest Rate on Mortgage' floatingLabelText='Interest Rate on Mortgage' />
-    <TextField hintText='Investment Rate' floatingLabelText='Investment Rate' />
-  </form>
-);
+export default ({state, handleChange}) => {
+  const {
+    salary,
+    taxRatio,
+    saveRate,
+    homeCost,
+    yearsBorrowed,
+    mortgageInterest,
+    investmentRate
+  } = state;
+
+  return <form>
+    <TextField id='salary' value={salary} onChange={handleChange} hintText='Base Salary' floatingLabelText='Base Salary' />
+    <TextField id='taxRatio' value={taxRatio} onChange={handleChange} hintText='Taxes and Deductions Ratio' floatingLabelText='Taxes and Deductions Ratio' />
+    <TextField id='saveRate' value={saveRate} onChange={handleChange} hintText='Savings Rate' floatingLabelText='Savings Rate' />
+    <TextField id='homeCost' value={homeCost} onChange={handleChange} hintText='Home Cost' floatingLabelText='Home Cost' />
+    <TextField id='yearsBorrowed' value={yearsBorrowed} onChange={handleChange} hintText='Years in Mortgage' floatingLabelText='Years in Mortgage' />
+    <TextField id='mortgageInterest' value={mortgageInterest} onChange={handleChange} hintText='Interest Rate on Mortgage' floatingLabelText='Interest Rate on Mortgage' />
+    <TextField id='investmentRate' value={investmentRate} onChange={handleChange} hintText='Investment Rate' floatingLabelText='Investment Rate' />
+  </form>;
+}
